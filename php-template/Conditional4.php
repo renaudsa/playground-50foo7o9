7150b37c250c29790eaf4455d5009d0e5c1d1f03
renaudsa <54441982+renaudsa@users.noreply.github.com>
2019-09-24 09:24:44 +0200

    Create Conditional4.php

---
+++ php-template/Conditional4.php
@@ -0,0 +1,13 @@
+<?php
+
+// Un policier fait passer un test d'alcoolémie
+// Si de l'alcool est détecté il répond non sinon il répond oui au conducteur pour partir
+function exercice4($alcool){
+  // $alcool vaut true s'il y a de l'alcool et false sinon
+  // écrivez la condition et retournez "oui" si alcool vaut true
+  // retournez "non" si alcool vaut false
+  
+  return "non";
+}
+
+?>
