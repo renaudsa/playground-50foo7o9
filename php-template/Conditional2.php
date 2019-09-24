6203b7aea50f963e933fad340e35c5a33005e8ac
renaudsa <54441982+renaudsa@users.noreply.github.com>
2019-09-24 09:23:12 +0200

    Create Conditional2.php

---
+++ php-template/Conditional2.php
@@ -0,0 +1,13 @@
+<?php
+
+// Cette fonction teste si la personne est majeure ou non pour l'autoriser à entrer sur un site
+function exercice2($age){
+  // Compléter la condition
+  if($age){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+?>
