cc7fdaad4adda503cdeae4765da342567d3f9a4b
renaudsa <54441982+renaudsa@users.noreply.github.com>
2019-09-24 10:13:26 +0200

    Delete Universe.php

--- php-template/Universe.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function countAllStars($galaxies) {
-        $totalStars = 0;
-        foreach ($galaxies as $star) {
-            $totalStars = $star; //Hint: Change = to +=
-        }
-        return $totalStars;
-    }
-?>
