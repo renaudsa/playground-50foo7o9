6d705babd46a6dc331fc4e0fd9aed887a3570cfc
renaudsa <54441982+renaudsa@users.noreply.github.com>
2019-09-24 09:27:34 +0200

    Create ConditionalTest4.php

---
+++ php-template/ConditionalTest4.php
@@ -0,0 +1,14 @@
+<?php
+include "Conditional.php";
+function test(){
+  // Test exercice 4
+  if(exercice4(true) == "non" && exercice4(false) == "oui"){
+    echo("TECHIO> success true \r\n");
+    echo("TECHIO> message --channel Bravo vous avez réussi l\'exercice 4\r\n");
+  }else{
+    echo("TECHIO> success false  \r\n");
+    echo("TECHIO> message --channel Oops l'exercice 4 ne fonctionne pas! Essayez encore 🐞 \r\n");
+  }
+}
+test();
+?>
