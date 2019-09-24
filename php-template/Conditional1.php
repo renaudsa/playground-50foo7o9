2bee35a2bdcdcded8f433e08f6bd284a0c3246dc
renaudsa <54441982+renaudsa@users.noreply.github.com>
2019-09-24 09:22:28 +0200

    Create Conditional1.php

---
+++ php-template/Conditional1.php
@@ -0,0 +1,13 @@
+<?php
+
+// Si la couleur est "bleu" retourner true et sinon retourner false
+function exercice1($couleur){
+  // Compléter la condition
+  if($couleur){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+?>
