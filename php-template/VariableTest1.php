31e474c99146f744fc298f30547bc08f25886397
renaudsa <54441982+renaudsa@users.noreply.github.com>
2019-09-26 13:08:04 +0200

    Create VariableTest1.php

---
+++ php-template/VariableTest1.php
@@ -0,0 +1,22 @@
+<?php
+
+include "Variable1.php";
+
+function test(){
+  ob_start(); // Start output buffering
+  exercice1();
+  $output = ob_get_contents(); // Store buffer in variable
+
+  ob_end_clean(); // End buffering and clean up
+
+  if($output = "Hello world"){
+    echo("TECHIO> success true \r\n");
+    echo("TECHIO> message --channel Bravo vous avez réussi l'exercice 1\r\n");
+  }else{
+    echo("TECHIO> success false  \r\n");
+    echo("TECHIO> message --channel Oops l'exercice 1 ne fonctionne pas! Essayez encore 🐞 \r\n");
+  }
+}
+test();
+
+?>
